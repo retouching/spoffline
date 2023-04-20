c8eec6fac707833f3dff5394f7652a68510df89d
arch <sylvain.lefeuvre0@gmail.com>
2023-04-20 12:27:01 +0200

    ✨ Can now get album tracks

--- spoffline/spotify.py
+++ spoffline/spotify.py
@@ -1,5 +1,7 @@
 import hashlib
+import math
 import os.path
+import time
 from contextlib import contextmanager
 
 import httpx
@@ -10,12 +12,13 @@ from librespot.metadata import TrackId
 from spoffline.cacher import Cacher
 from spoffline.configuration import config
 from spoffline.helpers.exceptions import SpotifyException
-from spoffline.models.song import Song, SongCover
+from spoffline.models.song import Song
 
 
 class Spotify:
     def __init__(self):
         self.cache = Cacher('spotify.api')
+        self._user_session = None
         os.makedirs(os.path.dirname(self.credentials_cache_path), exist_ok=True)
 
     @property
@@ -54,12 +57,22 @@ class Spotify:
 
         return req.json().get('access_token')
 
+    def get_api_session(self):
+        return httpx.Client(
+            headers={
+                'Authorization': f'Bearer {self.get_api_token()}'
+            }
+        )
+
     def get_user_session(self):
+        if self._user_session and self._user_session.is_valid():
+            return self._user_session
+
         if os.path.exists(self.credentials_cache_path):
             last_credentials = self.cache.get('credentials:hash') or ''
 
             if last_credentials == self.md5credentials:
-                session = Session.Builder(Session.Configuration(
+                self._user_session = Session.Builder(Session.Configuration(
                     stored_credentials_file=self.credentials_cache_path,
                     store_credentials=True,
                     cache_enabled=True,
@@ -67,12 +80,12 @@ class Spotify:
                     do_cache_clean_up=True,
                     retry_on_chunk_error=True,
                 )).stored_file(self.credentials_cache_path).create()
-                if session.is_valid():
-                    return session
+                if self._user_session.is_valid():
+                    return self._user_session
             else:
                 os.unlink(self.credentials_cache_path)
 
-        session = Session.Builder(Session.Configuration(
+        self._user_session = Session.Builder(Session.Configuration(
             stored_credentials_file=self.credentials_cache_path,
             store_credentials=True,
             cache_enabled=True,
@@ -86,17 +99,16 @@ class Spotify:
 
         self.cache.set('credentials:hash', self.md5credentials)
 
-        return session
+        return self._user_session
 
     def get_track_infos(self, track_id):
         song = self.cache.get(f'song:infos:{track_id}')
         if song:
             return song
 
-        with httpx.Client() as client:
+        with self.get_api_session() as client:
             req = client.get(
                 f'https://api.spotify.com/v1/tracks/{track_id}',
-                headers={'Authorization': f'Bearer {self.get_api_token()}'}
             )
 
         if req.status_code != httpx.codes.OK:
@@ -110,29 +122,84 @@ class Spotify:
             reverse=True
         ))).get('url')
 
-        with httpx.Client() as client:
-            req = client.get(image)
-
-        if req.status_code != httpx.codes.OK:
-            raise SpotifyException(f'Unable to fetch track cover (HTTP code: {req.status_code})')
-
         song = Song(
             id=data.get('id'),
             name=data.get('name'),
             artists=[a.get('name') for a in data.get('artists')],
             album=data.get('album').get('name'),
-            cover=SongCover(
-                data=req.content,
-                ext=req.headers.get('Content-Type').split('/').pop()
-            )
+            cover_url=image
         )
 
         self.cache.set(f'song:infos:{track_id}', song, 60 * 60 * 24 * 7)
 
         return song
 
+    def get_album_tracks(self, album_id):
+        songs = self.cache.get(f'album:{album_id}')
+        if songs:
+            for song in songs:
+                yield song
+            return
+
+        songs = []
+
+        with self.get_api_session() as client:
+            req = client.get(f'https://api.spotify.com/v1/albums/{album_id}')
+
+        if req.status_code != httpx.codes.OK:
+            raise SpotifyException(f'Unable to fetch album info (HTTP code: {req.status_code})')
+
+        data = req.json()
+
+        image = next(iter(sorted(
+            data.get('images'),
+            key=lambda i: i.get('height'),
+            reverse=True
+        ))).get('url')
+
+        max_pages = int(math.ceil(data.get('total_tracks') / float(50))) or 1
+
+        with self.get_api_session() as client:
+            for page in range(1, max_pages + 1):
+                req = client.get(
+                    f'https://api.spotify.com/v1/albums/{album_id}/tracks',
+                    params={
+                        'limit': 50,
+                        'offset': (page - 1) * 50
+                    }
+                )
+
+                if req.status_code != httpx.codes.OK:
+                    raise SpotifyException(f'Unable to fetch album tracks (HTTP code: {req.status_code})')
+
+                songs_data = req.json().get('items')
+
+                for song_data in songs_data:
+                    song = Song(
+                        id=song_data.get('id'),
+                        name=song_data.get('name'),
+                        artists=[a.get('name') for a in song_data.get('artists')],
+                        album=data.get('name'),
+                        cover_url=image
+                    )
+
+                    self.cache.set(f'song:infos:{song.id}', song, 60 * 60 * 24 * 7)
+                    songs.append(song)
+
+                    yield song
+
+                if page != max_pages:
+                    time.sleep(5)
+
+        self.cache.set(f'album:{album_id}', songs, 60 * 60 * 24 * 7)
+
     @contextmanager
     def get_track_content(self, track_id):
+        playable_id = self.cache.get(f'song:playable:{track_id}')
+        if not playable_id:
+            playable_id = TrackId.from_uri(f'spotify:track:{track_id}')
+            self.cache.set(f'song:playable:{track_id}', playable_id)
+
         stream = self.get_user_session().content_feeder().load(
             TrackId.from_uri(f'spotify:track:{track_id}'),
             VorbisOnlyAudioQuality(
